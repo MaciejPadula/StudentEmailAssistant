1b6501040ed283d59614bc41614cfb316b45f3ed
Maciej Padula <maciej.padula@wp.pl>
2024-05-29 17:30:25 +0200

    added client

--- src/Student.Email.Assistant.Backend/main.py
+++ src/Student.Email.Assistant.Backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import uvicorn
 from features.generate_title.generate_title_endpoint import router as generate_title_router
 from features.improve_content.improve_content_endpoint import router as improve_content_router
+import infrastructure.openai_client
 
 
 app = FastAPI()
