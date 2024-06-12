ca24994574c19691aa1223ad9e441a1ff402a4df
Maciej Padula <maciej.padula@wp.pl>
2024-06-12 17:21:41 +0200

    call to openai

--- src/Student.Email.Assistant.Backend/main.py
+++ src/Student.Email.Assistant.Backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from features.generate_title.generate_title_endpoint import router as generate_title_router
 from features.improve_content.improve_content_endpoint import router as improve_content_router
@@ -7,6 +8,15 @@ from features.improve_content.improve_content_endpoint import router as improve_
 app = FastAPI()
 app.include_router(generate_title_router)
 app.include_router(improve_content_router)
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def main():
     uvicorn.run(app, port=8080, host='127.0.0.1')
--- src/Student.Email.Assistant.Frontend/index.js
+++ src/Student.Email.Assistant.Frontend/index.js
@@ -1,16 +1,31 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
+const apiUrl = "http://127.0.0.1:8080";
+
 const link = document.createElement('link');
 link.rel = 'stylesheet';
 link.href = 'https://fonts.googleapis.com/icon?family=Material+Icons';
 document.head.appendChild(link);
 
-function addGenerateTitleButton(titleInput) {
+function addGenerateTitleButton(titleInput, contentInput) {
   const generateTitleButtonClass = "generate-title";
+  const inputValueContainer = titleInput.querySelector("input");
+  const contentInputValueContainer = contentInput.querySelector(".aO7 div");
   const btn = createIconButton(
     "psychology",
     generateTitleButtonClass,
-    () => {}
+    async () => {
+      const response = await fetch(`${apiUrl}/api/generate-title`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          "email_content": contentInputValueContainer.innerHTML
+        }),
+      });
+      inputValueContainer.value = await response.json();
+    }
   );
   btn.style.position = "absolute";
   btn.style.top = "0";
@@ -34,7 +49,7 @@ function addImproveEmailButton(contentInput) {
 }
 
 function processMailPopup(popup) {
-  addGenerateTitleButton(popup.titleInput);
+  addGenerateTitleButton(popup.titleInput, popup.contentInput);
   addImproveEmailButton(popup.contentInput);
 }
 
